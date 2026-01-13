d0f7681cba2f4465e79cea2c385a42c2b628369c
Abhay Bharti <abhi123@gmail.com>
2026-01-13 22:02:30 +0530

    String01

---
+++ src/StringPractice/PrintString.java
@@ -0,0 +1,14 @@
+package StringPractice;
+
+import java.util.Scanner;
+
+public class PrintString {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the String -> ");
+		String a = s.next();
+		System.out.println("String is -> "+a);
+	}
+
+}
